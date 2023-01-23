e2c9291b82bba5b5110e83234072978c09995629
jdw5 <joshuadwallace1@gmail.com>
2023-01-23 15:21:49 +1000

    Laravel echo and Dashboard page listener

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,6 +1,30 @@
-<script setup>
+<script>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
+
+export default {
+    components: {
+        AuthenticatedLayout,
+        Head
+    },
+
+    data() {
+        return {
+            messageReceived: false
+        }
+    },
+
+    mounted() {
+        Echo.channel('test')
+            .listen('WebsocketTest' ,(e) => {
+                this.messageReceived = true
+
+            })
+    }
+
+
+}
+
 </script>
 
 <template>
@@ -14,7 +38,9 @@ import { Head } from '@inertiajs/vue3';
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900">You're logged in!</div>
+                    <div class="p-6 text-gray-900">
+                        Message received? {{ messageReceived }}
+                    </div>
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Events\WebsocketTest;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -15,6 +16,10 @@ use Inertia\Inertia;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('/broadcast', function() {
+    broadcast(new WebsocketTest());
+});
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
